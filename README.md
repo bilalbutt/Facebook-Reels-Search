242d9e2b9be363ddfb002c0cca48ce9a6a21d3ed
Bilal Kabeer Butt <bilalbutt@gmail.com>
2026-09-07 17:05:04 +0500

    Initial code upload

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
+
 # Facebook Reels Search
 
+A simple JS script which searches the Facebook profile and highlights the reel that you were watching.
+
+## Why this was built
+
+Sometimes when you are going through the Facebook reels an interesting reel comes into view but it is divided into parts, sometime Facebook shows the button to go to that reel when we visit the user profile, sometimes it does n't. To handle this issue this script was written with which you can easily search for a Faceboook Reel from the user profile.
+
+## FAQ
+
+### How to use the script.
+
+- Open the user profile/page.
+- Goto "Reels" tab e,g. (https://www.facebook.com/facebook/reels/).
+- Open browser's "Developers Tools" (Google Chrome/Mozilla Firefox/Microsoft Edge)
+  - Press <kbd>F12</kbd> or <kbd>Ctrl</kbd> + <kbd>Shift</kbd> + <kbd>I</kbd>
+- Copy the script from Github, you can save the script on your system.
+- Open "Console" tab and paste the script and press enter.
+- 
---
+++ script.js
@@ -0,0 +1,67 @@
+// FACEBOOK REEL SEARCH
+// https://www.facebook.com/reel/913107821683246
+
+console.clear();
+let AskReelLnk = prompt("Enter Facebook Reel URL to Search.", "");
+var ReelLnk = "";
+var BorderColor = "#0866ff";
+var Style = 'border: 3px solid #000; font-size: 16px; padding: 10px; font-weight: bold; color: #fff; ';
+var StyleFound = Style + 'background: #00a12b;';
+var StyleNotFound = Style + 'background: #f00;';
+var Dots = ".";
+
+if (AskReelLnk != null) {
+    var match = AskReelLnk.match(/\/reel\/([a-zA-Z0-9]+)/);    
+    if (match) {
+        console.warn(match[1]);
+        ReelLnk = match[1];
+        // Clear All
+        window.scrollTo({top: 0});
+        document.querySelectorAll(".x9f619").forEach(function (parent) {
+            parent.querySelectorAll(":scope > div").forEach(function (child) {
+                child.style.border = "0px dashed " + BorderColor;
+            });
+        });
+        // Clear All
+        FindReel();
+    }
+}else{
+    alert("Please enter Facebook Reel URL.");
+}
+
+function FindReel(){
+	// FIND REEL & MAKE A BODER AND SCROLL TO IT
+	console.clear();
+	var ReelFound = false;
+	document.querySelectorAll(".x9f619").forEach(function (parent) {
+		parent.querySelectorAll(":scope > div").forEach(function (child) {
+			var lnk = child
+				.querySelector("div > a")
+				?.getAttribute("href");
+			//console.log(lnk);
+			if (!lnk) return;
+			var result = lnk.match(new RegExp(ReelLnk, "gi"));
+			if (result) {
+				//console.warn(result);
+				console.log("%c REEL FOUND!", StyleFound);
+				ReelFound = true;
+				child.style.border = "10px ridge " + BorderColor;
+				child.style.borderRadius = "20px";
+				window.scrollTo({
+					top: (parent.getBoundingClientRect().top + window.scrollY) - 140,
+					behavior: "smooth"
+				});
+			}
+		});
+	});
+	if (ReelFound) {
+        return;
+    }
+    console.log( "%c REEL NOT FOUND - SCROLLING FOR MORE" + Dots, StyleNotFound );
+	Dots = Dots + ".";	
+    window.scrollTo({ top: document.documentElement.scrollHeight });
+	setTimeout(FindReel, 5000);
+}
+
+/* */
+// FACEBOOK REEL SEARCH
